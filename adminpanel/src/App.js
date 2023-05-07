d1afe472abe7cc0bc74eb59a913d9e44d76b82d4
Mustafa <mmalowisi19@cit.just.edu.jo>
2023-05-08 01:29:27 +0300

    init new branch

--- adminpanel/src/App.js
+++ adminpanel/src/App.js
@@ -2,14 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import AddProduct from "./Pages/AddProduct";
 import routes from "./routes/routes";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      {/* {routes.map((route) => {
-    
-    })} */}
       <Route path="/" element={<AddProduct />} />
     </Routes>
   );
