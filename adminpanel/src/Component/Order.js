61766f45a13de14dea2a4f5dc8e670fe76195a47
Ahmad Zuiod <armousa19@cit.just.edu.jo>
2023-06-12 01:54:57 +0300

    DoneOrderPage and someChanges

--- adminpanel/src/Component/Order.js
+++ adminpanel/src/Component/Order.js
@@ -2,7 +2,10 @@ import React from "react";
 import DataTable from "../SharedComponent/DataTable";
 
 export default function Order(props) {
-  const Headers = ["OrderId","OrderState","setOrderState","Total Amount",]
- ;
-  return <><DataTable headerContent={Headers} bodyData={props.userOrders} /></>;
+  const Headers = ["OrderId", "OrderState", "setOrderState", "Total Amount"];
+  return (
+    <>
+      <DataTable headerContent={Headers} bodyData={props.userOrders} />
+    </>
+  );
 }
